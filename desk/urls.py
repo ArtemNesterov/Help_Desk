eb06da5ac7bef10d17e2009b75b60e60b36360dd
Your Name <artemnesterov1991@gmail.com>
2020-12-23 21:54:24 +0200

    add urls

--- desk/urls.py
+++ desk/urls.py
@@ -23,9 +23,14 @@ from help_desk import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('users.urls')),
+    path('', views.ClaimListView.as_view(), name='claims_list_page'),
+    path('help_desk/<int:pk>', views.ClaimDetailView.as_view(), name='claim_detail'),
+    path('help_desk/new', views.NewClaimView.as_view(), name='new_claim'),
+    path('help_desk/edit/<int:pk>', views.EditClaimView.as_view(), name='edit_claim'),
+    path('help_desk/delete/<int:pk>', views.ClaimDetailView.as_view(), name='claim_delete'),
+    path('comment/<int:pk>/new', views.NewCommentView.as_view(), name='new_comment'),
 
 
 
-    #path('claim_list', views.ClaimListView.as_view(template_name='claims_list.html'), name='claims_list_page'),
 
 ]
